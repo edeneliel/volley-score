dff7e0b16407df27a43b231c18a91c326de3d4f3
Eden Eliel <2xEshocK@gmail.com>
2022-11-18 19:42:04 +0200

    Should be fine

--- main.py
+++ main.py
@@ -1,97 +1,14 @@
-import re
-import requests
-from typing import List
-from docx import Document
-from bs4 import BeautifulSoup
-from docx.shared import Inches
+import strawberry
+from flask import Flask
+from api.base_query import Query
+from api.base_mutation import Mutation
+from strawberry.flask.views import GraphQLView
 
-from utilities.score_sheet import ScoreSheet
-from utilities.types.team import Team
-from utilities.types.game import Game
-from utilities.types.player import Player
-from docx.enum.section import WD_ORIENTATION
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 
+app = Flask(__name__)
+app.add_url_rule("/graphql", view_func=GraphQLView.as_view("graphql_view", schema=schema),)
 
-def start_game(home_team: Team, guest_team: Team, home_rotation: List[int], guest_rotation: List[int]):
-    game = Game(home_team_id=home_team.id, guest_team_id=guest_team.id)
-    game_set = game.start_set(home_rotation=home_rotation, guest_rotation=guest_rotation)
-
-    game.game_sets_ids.append(game_set.id)
-    game.save()
-
-    return game_set
-
-
-def simulate_game():
-    Team(id="1", name="Bardelas", players_ids=["1", "2", "3"]).save()
-    Team(id="2", name="Harish", players_ids=["a", "b", "c"]).save()
-    home_team = Team.get("1")
-    guest_team = Team.get("2")
-
-    game_set = start_game(home_team=home_team, guest_team=guest_team,
-                          home_rotation=[1, 6, 9, 5, 8, 3], guest_rotation=[12, 9, 1, 7, 10, 6])
-    # game_set = GameSet.get("bf53a623-6b09-4920-915b-61ea031324fc")
-
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(home_team.id)
-    game_set.give_point(guest_team.id)
-    game_set.give_point(home_team.id)
-
-
-def main():
-    # pass
-    simulate_game()
-
-    # ss = ScoreSheet()
-    # table = ss.add_set_table()
-    # table = ss.add_set_table()
-    # table = ss.add_set_table()
-    # ss.save()
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
