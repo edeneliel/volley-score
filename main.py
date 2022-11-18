f14fbb28af6f1348f84df6517cb537c1b5a80536
Eden Eliel <2xEshocK@gmail.com>
2022-11-18 19:54:40 +0200

    remove main

--- main.py
+++ main.py
@@ -9,6 +9,4 @@ schema = strawberry.Schema(query=Query, mutation=Mutation)
 app = Flask(__name__)
 app.add_url_rule("/graphql", view_func=GraphQLView.as_view("graphql_view", schema=schema),)
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+
